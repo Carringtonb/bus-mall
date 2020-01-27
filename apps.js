fdd5c0a010f721b1da40057578fb6996d6126908
carrington <carringtonbeard@live.com>
2020-01-27 09:43:23 -0800

    trying to fix images

--- apps.js
+++ apps.js
@@ -92,26 +92,26 @@ var handleClickOnPicture = function(event){
 };
 
 
-new Picture('bag', '/img/bag.jpg');
-new Picture('banana', '/img/banana.jpg');
-new Picture('bathroom', '/img/bathroom.jpg');
-new Picture('boots', '/img/boots.jpg');
-new Picture('breakfast', '/img/breakfast.jpg');
-new Picture('bubblegum', '/img/bubblegum.jpg');
-new Picture('chair', '/img/chair.jpg');
-new Picture('cthulhu', '/img/cthulhu.jpg');
-new Picture('dog-duck', '/img/dog-duck.jpg');
-new Picture('dragon', '/img/dragon.jpg');
-new Picture('pen', '/img/pen.jpg');
-new Picture('pet-sweep', '/img/pet-sweep.jpg');
-new Picture('scissors', '/img/scissors.jpg');
-new Picture('shark', '/img/shark.jpg');
-new Picture('sweep', '/img/sweep.png');
+new Picture('bag', 'img/bag.jpg');
+new Picture('banana', 'img/banana.jpg');
+new Picture('bathroom', 'img/bathroom.jpg');
+new Picture('boots', 'img/boots.jpg');
+new Picture('breakfast', 'img/breakfast.jpg');
+new Picture('bubblegum', 'img/bubblegum.jpg');
+new Picture('chair', 'img/chair.jpg');
+new Picture('cthulhu', 'img/cthulhu.jpg');
+new Picture('dog-duck', 'img/dog-duck.jpg');
+new Picture('dragon', 'img/dragon.jpg');
+new Picture('pen', 'img/pen.jpg');
+new Picture('pet-sweep', 'img/pet-sweep.jpg');
+new Picture('scissors', 'img/scissors.jpg');
+new Picture('shark', 'img/shark.jpg');
+new Picture('sweep', 'img/sweep.png');
 new Picture('tauntaun', 'img/tauntaun.jpg');
 new Picture('unicorn', 'img/unicorn.jpg');
 new Picture('usb', 'img/usb.gif');
-new Picture('water-can', '/img/water-can.jpg');
-new Picture('wine-glass', '/img/wine-glass.jpg');
+new Picture('water-can', 'img/water-can.jpg');
+new Picture('wine-glass', 'img/wine-glass.jpg');
 
 renderImage();
 
